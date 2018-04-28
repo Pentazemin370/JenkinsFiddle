35729181d5e671c94548ed1038ef495d5ad9f836
Dennis <dennishpark@gmail.com>
2018-04-27 22:47:59 -0400

    hello

--- jenkins-demo/src/main/java/servlets/TestServlet.java
+++ jenkins-demo/src/main/java/servlets/TestServlet.java
@@ -4,12 +4,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/test")
+
 public class TestServlet extends HttpServlet{
 	
 	private static long serialVersionUID = 1L;
